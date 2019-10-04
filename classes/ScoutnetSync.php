d8392db9536c8fa2540d628d22f1334412b4d12e
Philipp Lang <pille@zoomyboy.de>
2019-10-04 04:00:09 +0200

    Mod: Ensure that scoutnetSync is always constructed with a group

--- classes/ScoutnetSync.php
+++ classes/ScoutnetSync.php
@@ -6,48 +6,48 @@ use Zoomyboy\Scoutnet\Models\Calendar;
 use Zoomyboy\Scoutnet\Exceptions\CalendarNotFoundException;
 
 class ScoutnetSync {
-	private $calendarId;
-	private $sn;
+    private $calendarId;
+    private $sn;
 
-	public function __construct($calendarId) {
-		try {
-			$this->calendarId = $calendarId;
-			require_once(plugins_path() . '/zoomyboy/scoutnet/vendor/scoutnet-api-client-php/src/scoutnet.php');
-			$this->sn = scoutnet()->group($calendarId);
-		} catch(\SN_Exception_EndUser $e) {
-			if (str_contains($e->getMessage(), 'Es gibt kein group')) {
-				throw new CalendarNotFoundException();
-			}
-		}
+    private function __construct($calendarId) {
+        try {
+            $this->calendarId = $calendarId;
+            require_once(plugins_path() . '/zoomyboy/scoutnet/vendor/scoutnet-api-client-php/src/scoutnet.php');
+            $this->sn = scoutnet()->group($calendarId);
+        } catch(\SN_Exception_EndUser $e) {
+            if (str_contains($e->getMessage(), 'Es gibt kein group')) {
+                throw new CalendarNotFoundException();
+            }
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public static function fromGroup($calendarId) {
-		return new static($calendarId);
-	}
+    public static function fromGroup($calendarId) {
+        return new static($calendarId);
+    }
 
-	public function getName() {
-		return $this->sn->name;
-	}
+    public function getName() {
+        return $this->sn->name;
+    }
 
     public function isValid() {
         return $this->sn !== null;
     }
 
-	public function getZip() {
-		return $this->sn->zip;
-	}
+    public function getZip() {
+        return $this->sn->zip;
+    }
 
-	public function getCity() {
-		return $this->sn->city;
-	}
+    public function getCity() {
+        return $this->sn->city;
+    }
 
-	public function getId() {
-		return $this->sn->global_id;
-	}
+    public function getId() {
+        return $this->sn->global_id;
+    }
 
-	public function events() {
-		return new ScoutnetSyncEvents($this->sn);
-	}
+    public function events() {
+        return new ScoutnetSyncEvents($this->sn);
+    }
 }
