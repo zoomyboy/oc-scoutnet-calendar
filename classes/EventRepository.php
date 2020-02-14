3cba3f4a1a6deb3636f457b4ec22e27549f9032f
Philipp Lang <pille@zoomyboy.de>
2020-02-14 22:57:51 +0000

    Add date_format for dates

--- classes/EventRepository.php
+++ classes/EventRepository.php
@@ -18,7 +18,16 @@ class EventRepository {
 
         $query = (new Event())->newQuery()
             ->with(['keywords'])
-            ->select('title', 'calendar_id', 'organizer', 'starts_at', 'ends_at', 'location', 'target', 'id', 'url')
+            ->select('title', 'description', 'calendar_id', 'organizer', 'starts_at', 'ends_at', 'location', 'target', 'id', 'url', 'url_text')
+            ->selectSub('
+            IF(DATE_FORMAT(starts_at, "%Y%m%d") = DATE_FORMAT(ends_at, "%Y%m%d"), DATE_FORMAT(starts_at, "%d. %b"),
+            IF(DATE_FORMAT(starts_at, "%Y%m") = DATE_FORMAT(ends_at, "%Y%m"), CONCAT(DATE_FORMAT(starts_at, "%d."), " - ", DATE_FORMAT(ends_at, "%d. %b")),
+            IF(DATE_FORMAT(starts_at, "%Y") = DATE_FORMAT(ends_at, "%Y"), CONCAT(DATE_FORMAT(starts_at, "%d. %b"), " - ", DATE_FORMAT(ends_at, "%d. %b")),
+            NULL
+            )))', 'date')
+            ->selectSub('SUBSTRING(title, LOCATE(" - ", title))', 'subtitle')
+            ->selectSub('IF(LOCATE(" - ", title) != 0, SUBSTRING(title, LOCATE(" - ", title)+2), "")', 'subtitle')
+            ->selectSub('IF(LOCATE(" - ", title) != 0, SUBSTRING(title, 1, LOCATE(" - ", title)), title)', 'maintitle')
             ->selectRaw('(SELECT GROUP_CONCAT(zoomyboy_scoutnet_keywords.title SEPARATOR \', \') from zoomyboy_scoutnet_keywords WHERE zoomyboy_scoutnet_keywords.id IN (SELECT zoomyboy_scoutnet_event_keyword.keyword_id FROM zoomyboy_scoutnet_event_keyword WHERE zoomyboy_scoutnet_event_keyword.event_id=zoomyboy_scoutnet_events.id)) AS keywordList')
             ->where('starts_at', '>=', $minDate)
             ->orderBy('starts_at');
