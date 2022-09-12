3ed494474d8710670f02b0b822204d0b2d49f564
philipp lang <philipp@aweos.de>
2022-09-13 00:46:31 +0200

    Delete scoutnet events not found in scoutnet

--- classes/ScoutnetSync.php
+++ classes/ScoutnetSync.php
@@ -3,17 +3,19 @@
 namespace Zoomyboy\Scoutnet\Classes;
 
 use Zoomyboy\Scoutnet\Exceptions\CalendarNotFoundException;
-use Zoomyboy\Scoutnet\Models\Calendar;
+use Zoomyboy\Scoutnet\Models\Event;
 
-class ScoutnetSync {
+class ScoutnetSync
+{
     private $calendar;
     private $sn;
 
-    private function __construct($groupId, $calendar = null) {
+    private function __construct($groupId, $calendar = null)
+    {
         try {
             $this->calendar = $calendar;
             $this->sn = scoutnet()->group($groupId);
-        } catch(\SN_Exception_EndUser $e) {
+        } catch (\SN_Exception_EndUser $e) {
             if (str_contains($e->getMessage(), 'Es gibt kein group')) {
                 throw new CalendarNotFoundException();
             }
@@ -22,22 +24,29 @@ class ScoutnetSync {
         return $this;
     }
 
-    public static function fromGroup($groupId, $calendar = null) {
+    public static function fromGroup($groupId, $calendar = null)
+    {
         return new static($groupId, $calendar);
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->sn->name;
     }
 
-    public function sync() {
+    public function sync()
+    {
         $this->calendar->update(['title' => $this->getName()]);
 
-        $dateRange = [ date('Y') - 1, date('Y') + 1 ];
+        $dateRange = [date('Y') - 1, date('Y') + 1];
 
-        $events = collect($this->sn->events('start_date > "'.$dateRange[0].'-01-01" AND start_date < "'.$dateRange[1].'-12-31"'))->each(function($event) {
+        $response = collect($this->sn->events('start_date > "'.$dateRange[0].'-01-01" AND start_date < "'.$dateRange[1].'-12-31"'));
+        $events = $response->each(function ($event) {
             ScoutnetSyncEvent::sync($event, $this->calendar);
         });
-    }
 
+        $allIds = $response->map(fn ($event) => $event->id);
+
+        Event::where('calendar_id', $this->calendar->id)->whereNotIn('scoutnet_id', $allIds)->get()->each->delete();
+    }
 }
